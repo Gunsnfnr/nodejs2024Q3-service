929a8f1e65004c6dac862960acc5ec9afea8642e
Gunsnfnr <a.j.p.010203@gmail.com>
2024-11-12 01:35:47 +0300

    feat: add api documentation / swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { config } from 'dotenv';
 config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
 
 const port = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = await readFile('doc/api.json', { encoding: 'utf-8' });
+  console.log('document: ', document);
+  SwaggerModule.setup('doc', app, JSON.parse(document));
+
   await app.listen(port);
 }
 bootstrap();
