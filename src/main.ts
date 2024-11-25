663f1ee70c4e58ff7bbef65eef5d2a9f166491f2
Gunsnfnr <a.j.p.010203@gmail.com>
2024-11-26 00:24:05 +0300

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,41 @@
 import { config } from 'dotenv';
 config();
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { readFile } from 'fs/promises';
 import { Logger } from './services/logger/logger.service';
+import { AllExceptionsFilter } from './services/logger/exception-filter/exception-filter';
 
 const port = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: new Logger(),
+    // logger: new Logger(),
+    bufferLogs: true,
   });
+  const logger = new Logger();
+  app.useLogger(logger);
+
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   const document = await readFile('doc/api.json', { encoding: 'utf-8' });
   SwaggerModule.setup('doc', app, JSON.parse(document));
 
   await app.listen(port);
+
+  process.on('uncaughtException', (error: unknown) =>
+    logger.error(error as string),
+  );
+  process.on('unhandledRejection', (error: unknown) => {
+    logger.error(error as string);
+  });
+
+  // Uncomment to test uncaughtException handling:
+  // throw new Error('Test exception');
+
+  // Uncomment to test unhandledRejection handling:
+  // process.emit('rejectionHandled', Promise.reject('Test rejection'));
 }
 bootstrap();
-
-process.on('uncaughtException', (error) => console.log(error));
-process.on('unhandledRejection', (error) => console.log(error));
---
+++ src/services/logger/exception-filter/exception-filter.ts
@@ -0,0 +1,40 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { Logger } from '../logger.service';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const logger = new Logger();
+    logger.error(exception as string);
+
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      message:
+        exception instanceof HttpException
+          ? exception.message || 'Internal server error.'
+          : 'Unknown error.',
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
